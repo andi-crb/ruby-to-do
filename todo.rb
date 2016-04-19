5f0bf00c609a19b49790e940746df9dd3b58058b
andi-crb <andicbuchanan@gmail.com>
2016-04-19 14:54:26 +1200

    ability to add task

---
+++ todo.rb
@@ -0,0 +1,24 @@
+class List
+  attr_reader :list
+  attr_reader :all_tasks
+  def initialize
+    @all_tasks = []
+  end
+  def add(task)
+    all_tasks << task
+  end
+end
+
+class Task
+  attr_reader :description
+  def initialize(description)
+    @description = description
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  my_list = List.new
+  puts 'You have created a new list'
+  my_list.add(Task.new('Make Breakfast'))
+  puts 'You have added a task to the Todo List'
+end
